2e475c39a45b066383a8587aaefc5e2660f02f0f
Cody Sheppard <sheppard.c@live.com>
2014-12-07 19:37:35 -0800

    Reduce
    The reduce class and main class are implemented

--- src/edu/csupomona/cs499/hadoopCluster/yelpMapRed.java
+++ src/edu/csupomona/cs499/hadoopCluster/yelpMapRed.java
@@ -8,6 +8,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.io.*;
 
@@ -17,9 +19,7 @@ import org.json.simple.parser.ParseException;
 
 import java.util.*;
 
-import com.google.gson.*;
-
-public class yelpMapRed {
+public class yelpMapRed extends Configured implements Tool {
 
 	public static class Map extends
 			Mapper<Text, LongWritable, Text, LongWritable> {
@@ -48,7 +48,6 @@ public class yelpMapRed {
 								try {
 									context.write(name,rating);
 								} catch (InterruptedException e) {
-									// TODO Auto-generated catch block
 									e.printStackTrace();
 								}
 							}
@@ -65,9 +64,49 @@ public class yelpMapRed {
 
 		}
 	}
-
-	public static void main(String[] args) {
-
+	
+	public static class Reduce extends Reducer<Text,LongWritable,Text,LongWritable>{
+		public void reduce(Text key,Iterable<LongWritable> values,Context output) throws IOException, InterruptedException{
+			int count = 0;
+			long total = 0;
+			for (LongWritable value : values){
+				count++;
+				total += value.get();
+			}
+			total/=count;
+			output.write(key, new LongWritable(total));
+		}
 	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new yelpMapRed(), args);
+        System.exit(res); 
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("usage: [input] [output]");
+            System.exit(-1);
+        }
+
+        Job job = Job.getInstance(new Configuration());
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(yelpMapRed.class);
+
+        job.submit();
+        return 0;
+    }
 
 }
