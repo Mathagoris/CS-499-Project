355891c3584b7132ea9e8592ef1b473f758f11c3
Cody Sheppard <sheppard.c@live.com>
2014-12-07 17:01:56 -0800

    Fixed
    Errors in last

--- src/edu/csupomona/cs499/hadoopCluster/yelpMapRed.java
+++ src/edu/csupomona/cs499/hadoopCluster/yelpMapRed.java
@@ -1,6 +1,23 @@
 package edu.csupomona.cs499.hadoopCluster;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import java.io.*;
+import org.json.simple.*;
+import java.util.*;
+
 public class yelpMapRed {
+
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
